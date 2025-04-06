1a9f10f0120f5d3b9daecb580ce4c4d2778a4998
Mark Lester Echavez <marklester.echavez@avanade.com>
2025-04-06 13:46:48 -0400

    feat: use fast api request and response object

--- function_app.py
+++ function_app.py
@@ -1,31 +1,30 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+from azurefunctions.extensions.http.fastapi import Request, JSONResponse
 
 app = func.FunctionApp()
 
 
 @app.route(route="sample", auth_level=func.AuthLevel.FUNCTION)
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+async def http_trigger(req: Request) -> JSONResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get("name")
+    # name = req.params.get("name")
+    name = req.query_params.get("name")
     if not name:
         try:
-            req_body = req.get_json()
+            req_body = await req.json()
         except ValueError:
             pass
         else:
             name = req_body.get("name")
 
     if name:
-        return func.HttpResponse(
+        return JSONResponse(
             f"Hello, {name}. This HTTP triggered function executed successfully."
         )
     else:
-        return func.HttpResponse(
+        return JSONResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
             status_code=200,
         )
-
